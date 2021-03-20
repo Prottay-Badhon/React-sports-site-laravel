076f3d86621900ce6928374d80b633407ea3fdeb
Your Name <prottaybadhon54@gmail.com>
2021-03-21 00:21:25 +0600

    success

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        return $next($request)
         ->header('Access-Control-Allow-Origin',"*")
         ->header('Access-Control-Allow-Methods',"PUT,POST,DELETE,OPTIONS,GET")
         ->header('Access-Control-Allow-Headers',"Accept,Authorization,Content-Type");
